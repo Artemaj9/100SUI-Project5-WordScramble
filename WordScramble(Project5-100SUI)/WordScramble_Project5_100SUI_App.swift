040c218d1848873f62f3d865ad0a1bc97a2dd69b
Artem <moiseev@physics.msu.ru>
2023-07-24 15:32:52 +0300

    Initial Commit

---
+++ WordScramble(Project5-100SUI)/WordScramble_Project5_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_Project5_100SUI_App.swift
+//  WordScramble(Project5-100SUI)
+//
+//  Created by Artem on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_Project5_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
